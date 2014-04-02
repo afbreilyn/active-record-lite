de636322d9ab0d7f1377c25a781ad96b75a6650b
Chas McMahon <ChasMcMahon@appacademy.io>
2014-04-02 07:41:50 -0400

    Finishes

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -49,26 +49,29 @@ end
 module Associatable
   # Phase IVb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name,  options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
 
     define_method(name) do
 
+      options = self.class.assoc_options[name]
 #      debugger
        options
         .model_class
         .where(options.primary_key => self.send(options.foreign_key))
         .first
     end
-    # ...
+
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, self.name, options)
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
 
     define_method(name) do
 
+      options = self.class.assoc_options[name]
       options
-       .send[:model_class]
+       .model_class
        .where(options.foreign_key => self.send(options.primary_key))
 
     end
@@ -77,6 +80,8 @@ module Associatable
 
   def assoc_options
     # Wait to implement this in Phase V. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
--- lib/active_record_lite/05_associatable2.rb
+++ lib/active_record_lite/05_associatable2.rb
@@ -1,10 +1,39 @@
 require_relative '04_associatable'
-
+require 'debugger'
 # Phase V
 module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
+
+    define_method(name) do
+      #debugger
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_fk = through_options.foreign_key
+      through_pk = through_options.primary_key
+
+      source_table = source_options.table_name
+      source_fk = source_options.foreign_key
+      source_pk = source_options.primary_key
+
+      key = self.send(through_fk)
+      result = DBConnection.execute(<<-SQL, key)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_fk} = #{source_table}.#{source_pk}
+        WHERE
+          #{through_table}.#{through_pk} = ?
+      SQL
+
+      source_options.model_class.parse_all(result).first
+    end
   end
 end
